3e6699bad854afb56df75579834171ad63ad74ae
robM <robm@student.edu>
2017-12-20 17:46:53 +0100

    scrape_div_with_bs

--- access.py
+++ access.py
@@ -5,26 +5,25 @@ import requests
 
 s = requests.session()
 
-response = s.get('https://www.olx.pl/konto/?ref%5B0%5D%5Baction%5D=myaccount&ref%5B0%5D%5Bmethod%5D=index')
+s.get('https://www.olx.pl/konto/?ref%5B0%5D%5Baction%5D=myaccount&ref%5B0%5D%5Bmethod%5D=index')
 
 # now post to that login page with some valid credentials and the token
-
+# from confing import zmienne
 auth = {
     'login[email]': 't411701@mvrht.net'
     , 'login[password]': 'cobin4hood4'
 }
 s.post('https://www.olx.pl/konto/?ref%5B0%5D%5Baction%5D=myaccount&ref%5B0%5D%5Bmethod%5D=index', data=auth)
 
-# now we should be authenticated, try visiting a protected page
-response = s.get('https://www.olx.pl/konto/?ref%5B0%5D%5Baction%5D=myaccount&ref%5B0%5D%5Bmethod%5D=index')
 
+# now we should be authenticated, try visiting a protected page
+# response = s.get('https://www.olx.pl/konto/?ref%5B0%5D%5Baction%5D=myaccount&ref%5B0%5D%5Bmethod%5D=index')
 # print(response.text)
 
-    
+
 source_code = s.get('https://www.olx.pl/mojolx/odpowiedzi')
 plain_text = source_code.text
 soup = BeautifulSoup(plain_text, "html.parser")
-
 
 # print(soup.find_all('div', {"class": "color-1 fnormal desc brkword"}))
 
