3ebc141242efa45b654d998850aac1a97d41afe3
malin4 <2rmalinowski@gmail.com>
2018-02-02 17:15:01 +0100

    starting application

--- my_interface/my_interface/settings.py
+++ my_interface/my_interface/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
-from auth import secret_key, pg_pass
+from auth import skey_myblog, pg_pass
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secret_key
+SECRET_KEY = skey_myblog
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
