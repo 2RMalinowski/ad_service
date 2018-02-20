3e78ef1929e720d9abeeb976af6ad6a762d28ca9
malin4 <2rmalinowski@gmail.com>
2018-02-20 17:28:15 +0100

    during_deploy

--- ui_app/models.py
+++ ui_app/models.py
@@ -5,16 +5,17 @@ from django.utils import timezone
 
 class Message(models.Model):
     ANSWER_CHOICES = (
-        ('ans_this_week', 'twk'),
-        ('ans_this_week_u18', 'twu18'),
-        ('ans_ms_this_week', 'mstwk'),
-        ('ans_mr_this_week', 'mrtwk'),
-        ('ans_next_week', 'nwk'),
-        ('ans_next_week_u18', 'nwu18'),
-        ('ans_ms_next_week', 'msnwk'),
-        ('ans_mr_next_week', 'mrnwk'),
+        ('ans_this_wk', 'this_week'),
+        ('ans_this_wk<18', 'this_week<18'),
+        ('ans_ms_this_wk', 'ms_this_week'),
+        ('ans_mr_this_wk', 'mr_this_week'),
+        ('ans_next_wk', 'next_week'),
+        ('ans_next_wk<18', 'next_week<18'),
+        ('ans_ms_next_wk', 'ms_next_week'),
+        ('ans_mr_next_wk', 'mr_next_week'),
     )
     selected = models.BooleanField(default=False)
     author = models.CharField(max_length=50)
     ad_body = models.TextField()
     ad_date = models.DateTimeField(default=timezone.now)
+    answer = models 
