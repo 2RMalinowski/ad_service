0bf5bb70fca85b4491fbe93b08399ad3f35f31ba
malin4 <2rmalinowski@gmail.com>
2018-02-20 20:36:43 +0100

    during_deploy

--- ui_app/models.py
+++ ui_app/models.py
@@ -1,9 +1,12 @@
+from answers import ans_1
 from django.db import models
 # from django.contrib.auth.models import User
 from django.utils import timezone
+ans_this_wk = ans_1
 
 
 class Message(models.Model):
+
     ANSWER_CHOICES = (
         ('ans_this_wk', 'this_week'),
         ('ans_this_wk<18', 'this_week<18'),
@@ -14,8 +17,9 @@ class Message(models.Model):
         ('ans_ms_next_wk', 'ms_next_week'),
         ('ans_mr_next_wk', 'mr_next_week'),
     )
+    
     selected = models.BooleanField(default=False)
     author = models.CharField(max_length=50)
     ad_body = models.TextField()
     ad_date = models.DateTimeField(default=timezone.now)
-    answer = models.TextField(choices=ANSWER_CHOICES)
+    answer = models.TextField(choices=ANSWER_CHOICES, default='ans_this_wk')
