05a43ccb95c79cf3a750b9cd2e5440aaa6f42bf4
malin4 <2rmalinowski@gmail.com>
2018-03-05 22:41:20 +0100

    answers_templates

--- ui_app/models.py
+++ ui_app/models.py
@@ -16,12 +16,21 @@ class Message(models.Model):
     ANSWER_CHOICES = (
         ('ans_this_wk', 'this_week'),
         ('ans_this_wk<18', 'this_week<18'),
-        ('ans_ms_this_wk', 'ms_this_week'),
         ('ans_mr_this_wk', 'mr_this_week'),
+        ('ans_ms_this_wk', 'ms_this_week'),
         ('ans_next_wk', 'next_week'),
         ('ans_next_wk<18', 'next_week<18'),
-        ('ans_ms_next_wk', 'ms_next_week'),
         ('ans_mr_next_wk', 'mr_next_week'),
+        ('ans_ms_next_wk', 'ms_next_week'),
+        ('inv_m_orday', 'inv_m'),
+        ('inv_m_wknday', 'inv_m_wknd'),
+        ('inv_f_orday', 'inv_f'),
+        ('inv_f_wknday', 'inv_f_wknd'),
+        ('inv_mr_orday', 'inv_mr'),
+        ('inv_mr_wknday', 'inv_mr_wknd'),
+        ('inv_ms_orday', 'inv_ms'),
+        ('inv_ms_wknday', 'inv_ms_wknd'),
+
     )
 
     selected = models.BooleanField(default=False)
@@ -33,4 +42,11 @@ class Message(models.Model):
     status = models.CharField(max_length=10, default='incoming')
     objects = models.Manager()
     incoming = MessageManager()
+
+
+class Answer(models.Model):
+    s
+
+
+
 
