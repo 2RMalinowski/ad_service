a9d273cf0af7be1974dccf07390443cfd1324b6d
malin4 <2rmalinowski@gmail.com>
2018-03-09 11:46:05 +0100

    answers templates added

--- ui_app/admin.py
+++ ui_app/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Answer
 
-admin.site.register(Answer)
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('template_name', 'created')
+    list_filter = ('template_name', 'created')
+    search_fields = ('template_name', 'template_body')
+    date_hierarchy = 'created'
+    ordering = ['created']
+
+
+admin.site.register(Answer, AnswerAdmin)
