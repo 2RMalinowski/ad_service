0c26f788cd76015ea84ab819d48c2cc6f782aaa4
malin4 <2rmalinowski@gmail.com>
2018-03-13 15:22:28 +0100

    changing templates

--- ad_service/settings.py
+++ ad_service/settings.py
@@ -5,7 +5,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# TEMP_DIR = os.path.join(BASE_DIR, 'templates')
+TEMP_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -47,8 +47,7 @@ ROOT_URLCONF = 'ad_service.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        # 'TEMP_DIR'
+        'DIRS': ['ui_app'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
